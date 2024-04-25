ede415ba63e3e8884b4ac1bbf5e8f7122f899161
shreyasprabhakar <shrprabh@ttu.edu>
2024-04-25 16:43:33 -0500

    feature(category):sending categories to view

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -21,7 +21,7 @@ namespace BulkyWeb.Controllers
         public IActionResult Index()
         {
             List<Category> objCategoryList = _db.Categories.ToList();
-            return View();
+            return View(objCategoryList);
         }
 
     }
