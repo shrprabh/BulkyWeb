e13f0f8148dc75c4fc5d5d2da1b218b2f273e864
shreyasprabhakar <shrprabh@ttu.edu>
2024-04-25 16:28:27 -0500

    feature(category):adding data to database table category by migration command

--- BulkyWeb/Data/ApplicationDbContext.cs
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -1,18 +1,28 @@
-﻿using System;
-using BulkyWeb.Models;
-using Microsoft.EntityFrameworkCore;
+﻿	using System;
+	using BulkyWeb.Models;
+	using Microsoft.EntityFrameworkCore;
+	using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace BulkyWeb.Data
-{
-	public class ApplicationDbContext:DbContext
+	namespace BulkyWeb.Data
 	{
-
-		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+		public class ApplicationDbContext:DbContext
 		{
 
-		}
+			public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+			{
 
-		public DbSet<Category> Categories { get; set; }
+			}
+
+			public DbSet<Category> Categories { get; set; }
+			protected override void OnModelCreating(ModelBuilder modelBuilder)
+			{
+				modelBuilder.Entity<Category>().HasData(
+					new Category { CategoryId = 101, Name = "Action", DisplayOrder = 1 },
+					new Category { CategoryId = 102, Name = "SciFi", DisplayOrder = 2 },
+					new Category { CategoryId = 103, Name = "History", DisplayOrder = 3 }
+					);
+			}
+
+		}
 	}
-}
 
