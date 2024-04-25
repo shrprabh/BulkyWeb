abb9ace2734597f360f85492ab4b8d9ca05eb391
shreyasprabhakar <shrprabh@ttu.edu>
2024-04-25 16:28:43 -0500

    feat(category);MigratioFiles

---
+++ BulkyWeb/Migrations/20240425212514_AddCategoryDataToCategoryTable.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BulkyWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoryDataToCategoryTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "DisplayOrder", "Name" },
+                values: new object[,]
+                {
+                    { 101, 1, "Action" },
+                    { 102, 2, "SciFi" },
+                    { 103, 3, "History" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 101);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 102);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 103);
+        }
+    }
+}
