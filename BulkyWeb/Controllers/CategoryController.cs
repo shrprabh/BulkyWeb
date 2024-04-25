c03b7651a93b545e9e064f3001da94510bc45033
shreyasprabhakar <shrprabh@ttu.edu>
2024-04-25 16:37:05 -0500

    feature(retrieve):retrieve all categories in controller

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BulkyWeb.Data;
+using BulkyWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,11 +12,18 @@ namespace BulkyWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         // GET: /<controller>/
         public IActionResult Index()
         {
+            List<Category> objCategoryList = _db.Categories.ToList();
             return View();
         }
+
     }
 }
 
